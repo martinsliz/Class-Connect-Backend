1df500d8f3a40a95b902bd3a5424ac48eef97aee
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-03-03 12:21:58 -0500

    setup models

---
+++ migrations/20230303171222-create-class.js
@@ -0,0 +1,43 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      semester: {
+        type: Sequelize.STRING
+      },
+      teacher: {
+        type: Sequelize.STRING
+      },
+      credits: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('classes')
+  }
+}
