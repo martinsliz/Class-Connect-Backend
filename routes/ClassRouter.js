9b9640cc2ab880dc6710d51b6036d0e200c6a560
Willard Szeto <wszeto11@gmail.com>
2023-03-06 11:53:00 -0500

    Full CRUD on comments

--- routes/ClassRouter.js
+++ routes/ClassRouter.js
@@ -1,30 +1,30 @@
 const router = require('express').Router()
 const controller = require('../controllers/ClassController')
-// const middleware = require('../middleware')
+const middleware = require('../middleware')
 
 router.get('/', controller.GetClasses)
 router.post(
   '/',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.CreateClass
 )
 router.get(
   '/:class_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.GetClassById
 )
 router.put(
   '/:class_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.UpdateClass
 )
 router.delete(
   '/:class_id',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.DeleteClass
 )
 
